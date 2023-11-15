092c739f6f69cb74ba286b3dd2803da393ffc4a2
BrosephMC <jojoglick418@gmail.com>
2023-11-15 14:01:37 -0600

    prepare for model

--- teapot_main.js
+++ teapot_main.js
@@ -22,27 +22,6 @@ cube.position.y -= 2.5;
 scene.add( cube );
 // -- Cube end
 
-// -- Lines
-// const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-// camera.position.set( 0, 0, 100 );
-// camera.lookAt( 0, 0, 0 );
-
-// const scene = new THREE.Scene();
-
-// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-// const points = [];
-// points.push( new THREE.Vector3( - 10, 0, 0 ) );
-// points.push( new THREE.Vector3( 0, 10, 0 ) );
-// points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-// const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-// const line = new THREE.Line( geometry, material );
-
-// scene.add( line );
-// -- Lines end
-
 // -- 3D models
 // import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
