af04991b2c41610aaae317efc3b8f12206bd4eb6
BrosephMC <jojoglick418@gmail.com>
2023-11-15 23:00:30 -0600

    changes

--- teapot_main.js
+++ teapot_main.js
@@ -17,7 +17,6 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.m
 import { TeapotGeometry } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/geometries/TeapotGeometry.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls  } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/OrbitControls.js';
-import { ImprovedNoise } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/math/ImprovedNoise.js';
 
 const mapSize = 300
 
